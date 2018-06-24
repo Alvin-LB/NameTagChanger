a2fa8db5456d82728fc7ed960c4e0c65732b9bcd
Xesau <Xesau@users.noreply.github.com>
2018-06-24 18:11:53 +0200

    Prevent ConcurrentModificationEx. in disable() (#4)
    * Prevent ConcurrentModificationEx. in disable()
    
    ConcurrentModificationExceptions can occur when a HashMap is modified while being iterated through. By first wrapping the gameProfiles.keySet() in an ArrayList, the HashMap is not modified while iterating through it. I'm not sure if this is the perfect solution, but it should do the job just fine.

--- src/main/java/com/bringholm/nametagchanger/NameTagChanger.java
+++ src/main/java/com/bringholm/nametagchanger/NameTagChanger.java
@@ -328,7 +328,8 @@ public class NameTagChanger {
      */
     public void disable() {
         Validate.isTrue(enabled, "NameTagChanger is already disabled");
-        for (UUID uuid : gameProfiles.keySet()) {
+        // Prevent ConcurrentModificationException by wrapping keySet in a list
+        for (UUID uuid : new ArrayList<>(gameProfiles.keySet())) {
             Player player = Bukkit.getPlayer(uuid);
             if (player == null) {
                 continue;
